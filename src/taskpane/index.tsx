53a939b1f45cf7e4f5eb965f2bed59e18ae467d6
HarshitBhadouria <50481054+HarshitBhadouria@users.noreply.github.com>
2021-11-01 14:02:40 +0530

    Add files via upload

---
+++ src/taskpane/index.tsx
@@ -0,0 +1,55 @@
+import App from "./components/App";
+import { AppContainer } from "react-hot-loader";
+import { initializeIcons } from "@fluentui/font-icons-mdl2";
+import { ThemeProvider } from "@fluentui/react";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+/* global document, Office, module, require */
+
+initializeIcons();
+
+let isOfficeInitialized = false;
+
+const title = "Contoso Task Pane Add-in";
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <ThemeProvider>
+        <Component title={title} isOfficeInitialized={isOfficeInitialized} />
+      </ThemeProvider>
+    </AppContainer>,
+    document.getElementById("container")
+  );
+};
+
+/* Render application after Office initializes */
+Office.initialize = () => {
+  isOfficeInitialized = true;
+  render(App);
+};
+
+if ((module as any).hot) {
+  (module as any).hot.accept("./components/App", () => {
+    const NextApp = require("./components/App").default;
+    render(NextApp);
+  });
+}
+
+Excel.run(function (context) {
+  var sheet = context.workbook.worksheets.getItem("Sample");
+  var expensesTable = sheet.tables.add("A1:D1", true /*hasHeaders*/);
+  expensesTable.name = "Entity";
+
+  expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
+
+
+  if (Office.context.requirements.isSetSupported("ExcelApi", "1.2")) {
+      sheet.getUsedRange().format.autofitColumns();
+      sheet.getUsedRange().format.autofitRows();
+  }
+
+  sheet.activate();
+
+  return context.sync();
+})
